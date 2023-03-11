c5138c3646febac14d243c6c31f448839443b4f7
StaragileDevops <116961807+StaragileDevops@users.noreply.github.com>
2023-03-11 19:25:44 +0530

    Delete src directory

--- src/main/java/com/project/Insurance/PolicyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.Insurance;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PolicyRepository extends JpaRepository<Policy,Integer> {
-
-}
